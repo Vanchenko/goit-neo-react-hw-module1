e73d7e4bd0151ea330bc9c919c98fc0555460025
Yevhen <vanchenkoev@pakline.com.ua>
2025-12-01 11:14:13 +0200

    new file TransactionHistory.jsx

---
+++ src/components/TransactionHistory/TrasactionHistory.jsx
@@ -0,0 +1,22 @@
+import css from './TransactionHistory.module.css';
+
+const TransactHistory = ({ items }) => (
+    <table className={css.transactHistory}>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead> 
+        <tbody>    
+        {items.map(elem => (<tr key={elem.id} className={css.item}>
+            <td>{elem.type}</td>
+            <td>{elem.amount}</td>
+            <td>{elem.currency}</td>
+        </tr>))}
+       </tbody>         
+  </table>
+);
+
+export default TransactHistory;
